dfd947c026eb62f190d5325c4d3e766d9977f6cf
manggala-patra <manggala.kluk13@gmail.com>
2026-04-04 23:11:40 +0700

    Add files via upload

---
+++ Modul 3/soal1.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func faktorial(n int) int {
+	hasil := 1
+	for i := 1; i <= n; i++ {
+		hasil *= i
+	}
+	return hasil
+}
+
+func permutation(n, r int) int {
+	return faktorial(n) / faktorial(n-r)
+}
+
+func combination(n, r int) int {
+	return faktorial(n) / (faktorial(r) * faktorial(n-r))
+}
+
+func main() {
+	var a, b, c, d int
+	fmt.Scan(&a, &b, &c, &d)
+
+	fmt.Println(permutation(a, c), combination(a, c))
+	fmt.Println(permutation(b, d), combination(b, d))
+}

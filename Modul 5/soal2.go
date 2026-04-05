954525769925c8153d616516e17279c4409de569
manggala-patra <manggala.kluk13@gmail.com>
2026-04-05 20:48:14 +0700

    Add files via upload

---
+++ Modul 5/soal2.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func cetakBintang(n int, i int) {
+	if i > n {
+		return
+	}
+
+	for j := 0; j < i; j++ {
+		fmt.Print("*")
+	}
+	fmt.Println()
+
+	cetakBintang(n, i+1)
+}
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+
+	cetakBintang(n, 1)
+}
